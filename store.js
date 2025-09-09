174b64d89f50d942ec584a32f4409fcc3440fa91
mernacademy2024 <mernacademy2024@gmail.com>
2025-09-09 23:32:40 +0530

    merge queryslice and normal asynchThunk slice

--- store.js
+++ store.js
@@ -1,12 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { api } from "./apiSlice";
+import { api } from "./apiSlice";       // 1st API
+import { userApi } from "./userApi";   // 2nd API
+import authReducer from "./authSlice"; // Normal slice
 
 export const store = configureStore({
     reducer: {
-        [api.reducerPath]: api.reducer,
+        [api.reducerPath]: api.reducer,         // RTK Query API reducer 1
+        [userApi.reducerPath]: userApi.reducer, // RTK Query API reducer 2
+        auth: authReducer,                      // Normal slice
     },
     middleware: (getDefaultMiddleware) => [
         ...getDefaultMiddleware(),
         api.middleware,
+        userApi.middleware,
     ],
 });
